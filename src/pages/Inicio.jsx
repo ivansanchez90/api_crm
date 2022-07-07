6b44bc63d8a24f3b0bd4aba401bf2acb977d6d4d
ivansanchez90 <sanchezoliva.ivan@gmail.com>
2022-07-07 09:51:49 -0300

    agregando dependencia json server, comenzando creacion crud

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
 
 const Inicio = () => {
+
+  const [clientes, setClientes] = useState([])
+
+
+  useEffect(() => {
+    const obtenerClientesApi = async () => {
+      try {
+        const url = 'http://localhost:4000/clientes'
+        const respuesta = await fetch(url)
+        const resultado = await respuesta.json()
+        setClientes(resultado);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    obtenerClientesApi()
+  }, [])
+  
+
   return (
     <div>
         <h1>Inicio.jsx</h1>
