cae0e0da6e6a7976722883b08052f290806fdf91
ivansanchez90 <sanchezoliva.ivan@gmail.com>
2022-07-08 17:01:35 -0300

    deployment

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,4 +1,5 @@
 import {useState, useEffect} from 'react'
+import Cliente from '../components/Cliente'
 
 const Inicio = () => {
 
@@ -19,10 +20,51 @@ const Inicio = () => {
     obtenerClientesApi()
   }, [])
   
+  const handleEliminar = async id => {
+    const confirmar = confirm('Deseas eliminar éste cliente?')
+    if(confirmar){
+      try {
+        const url = `http://localhost:4000/clientes/${id}`
+        const respuesta = await fetch(url, {
+          method: 'DELETE'
+        })
+          await respuesta.json()
+          const arrayClientes = clientes.filter( cliente => cliente.id !== id)
+          setClientes(arrayClientes)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
 
   return (
     <div>
-        <h1>Inicio.jsx</h1>
+         <>
+        <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
+        <p className="mt-3" >Administra tus clientes</p>
+        
+
+        <table className="w-full mt-5 table-auto shadow bg-white">
+            <thead className="bg-blue-800 text-white">
+                <tr>
+                  <th className="p-2">Nombre</th>
+                  <th className="p-2">Contacto</th>
+                  <th className="p-2">Empresa</th>
+                  <th className="p-2">Acciones</th>
+                </tr>
+            </thead>
+
+            <tbody>
+              {clientes.map(cliente => (
+                <Cliente
+                key={cliente.id}
+                cliente={cliente}
+                handleEliminar={handleEliminar}
+                />
+              ))}
+            </tbody>
+        </table>
+    </>
     </div>
   )
 }
